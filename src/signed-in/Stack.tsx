ee4ee224bfe2c95a7d79f777cf41a190f5ecf290
Christopher Da Silva <mr.chrisdasilver@gmail.com>
2020-03-02 18:10:21 -0500

    auth

---
+++ src/signed-in/Stack.tsx
@@ -0,0 +1,35 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {Theme, withTheme} from 'react-native-paper';
+import Profile from './Profile';
+import Settings from './Settings';
+
+interface Props {
+  theme: Theme;
+}
+
+const Stack = createStackNavigator();
+
+function SignedInStack({theme}: Props) {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {
+            backgroundColor: theme.colors.primary,
+          },
+          headerTintColor: theme.colors.accent,
+        }}>
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+        />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default withTheme(SignedInStack);
---
+++ src/signed-out/Stack.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Theme, withTheme} from 'react-native-paper';
+import CreateAccount from './CreateAccount';
+import ForgotPassword from './ForgotPassword';
+import PhoneSignIn from './PhoneSignIn';
+import SignIn from './SignIn';
+
+interface Props {
+  theme: Theme;
+}
+
+const Stack = createStackNavigator();
+
+function SignedOutStack({theme}: Props) {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          // headerShown: false,
+          headerStyle: {
+            backgroundColor: theme.colors.primary,
+          },
+          headerTintColor: theme.colors.accent,
+        }}>
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+        />
+        <Stack.Screen
+          name="CreateAccount"
+          options={{title: 'Create Account'}}
+          component={CreateAccount}
+        />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPassword}
+          options={{title: 'Forgot Password'}}
+        />
+        <Stack.Screen name="PhoneSignIn" component={PhoneSignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default withTheme(SignedOutStack);
