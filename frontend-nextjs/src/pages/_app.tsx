87a795e3499ccb5f2f05f00be45a16aa5aab85ec
chris9753 <mr.chrisdasilver@gmail.com>
2025-02-26 14:17:19 -0300

    Add toast notifications for user feedback in the app layout

--- frontend-nextjs/src/pages/_app.tsx
+++ frontend-nextjs/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
@@ -11,6 +12,30 @@ export default function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#fff',
+            color: '#363636',
+          },
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: '#059669',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            duration: 5000,
+            iconTheme: {
+              primary: '#dc2626',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
